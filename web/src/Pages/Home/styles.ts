9ceb235d224491637de29ba627ce0be6662c06c6
NoronhaGCP <inc.691@gmail.com>
2021-03-08 21:01:25 -0300

    add background image on home

--- web/src/Pages/Home/styles.ts
+++ web/src/Pages/Home/styles.ts
@@ -1,5 +1,19 @@
 import styled from 'styled-components';
 
+import BackgroundIMG from '../../assets/background-img.jpg';
+
 export const Container = styled.div`
-  
+    overflow: hidden;
 `;
+
+export const BackgroundImg = styled.div`
+    position: absolute;
+    filter: brightness(.4);
+    width: 100%;
+    height: 75%;
+    z-index: -10;
+
+    background-image: url(${ BackgroundIMG });
+    background-size: cover;
+    background-position-y: -170px;
+`
