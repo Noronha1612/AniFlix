c89568c1572a8d9abbc53452214083b76bfa3be2
NoronhaGCP <inc.691@gmail.com>
2021-03-08 21:42:09 -0300

    add main content on first section of home

--- web/src/Pages/Home/index.tsx
+++ web/src/Pages/Home/index.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { BackgroundImg, Container } from './styles';
+import { FirstSection, Container } from './styles';
 
 import Header from './Components/Header';
 
 const Home: React.FC = () => {
     return (
         <Container>
-            <BackgroundImg />
-            
-            <Header />
+            <FirstSection>
+                <Header />
+
+                <main>
+                    <section>
+                        <h1>Filmes, séries e muito mais. Sem limites.</h1>
+                        <h4>Assista onde quiser. Cancele quando quiser.</h4>
+                        <p>Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura.</p>
+
+                        <div>
+                            <input type="text" placeholder="Entrar" />
+                            <button>
+                                Vamos lá
+                                <FiChevronRight className="icon" />
+                            </button>
+                        </div>
+                    </section>
+                </main>
+            </FirstSection>
         </Container>
     );
 }
--- web/src/Pages/Home/styles.ts
+++ web/src/Pages/Home/styles.ts
@@ -1,19 +1,102 @@
 import styled from 'styled-components';
 
 import BackgroundIMG from '../../assets/background-img.jpg';
+import { mainRed } from '../../styles/variables/colors';
 
 export const Container = styled.div`
     overflow: hidden;
 `;
 
-export const BackgroundImg = styled.div`
-    position: absolute;
-    filter: brightness(.4);
+export const FirstSection = styled.div`
     width: 100%;
-    height: 75%;
-    z-index: -10;
+    height: 77.5vh;
 
-    background-image: url(${ BackgroundIMG });
-    background-size: cover;
-    background-position-y: -170px;
+    border-bottom: 8px solid #222222;
+
+    position: relative;
+
+    &::before {
+        content: '';
+        width: 100%;
+        height: 100%;
+        filter: brightness(.4);
+        position: absolute;
+        background-image: url(${ BackgroundIMG });
+        background-size: cover;
+        background-position-y: -170px;
+        z-index: -10;
+    }
+
+    main {
+        width: 100%;
+        height: 100%;
+
+        padding: 0 20px;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+
+        transform: translateY(-60px);
+
+        section {
+            width: 100%;
+            max-width: 700px;
+            text-align: center;
+
+            h1 {
+                font-size: 4em;
+                margin-bottom: 8px;
+            }
+            h4 {
+                font-size: 1.625em;
+                font-weight: 500;
+                margin-bottom: 32px;
+            }
+            p {
+                font-size: 1.17em;
+                margin-bottom: 16px;
+            }
+
+            div {
+                width: 100%;
+                height: 70px;
+
+                display: flex;
+                justify-content: center;
+                align-items: center;
+
+                input {
+                    width: 70%;
+                    height: 100%;
+                    border: none;
+
+                    font-size: 1em;
+
+                    padding: 0 12px;
+
+                    color: #000;
+                }
+                button {
+                    width: 30%;
+                    height: 100%;
+                    border: none;
+
+                    font-size: 30px;
+
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+
+                    background: ${ mainRed };
+                    border-left: .25px solid #444;
+
+                    .icon {
+                        margin-left: 8px;
+                    }
+                }
+            }
+        }
+    }
 `
