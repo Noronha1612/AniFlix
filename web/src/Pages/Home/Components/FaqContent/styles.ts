ad7284a8b13264d02beefca86de69efb8806ce54
NoronhaGCP <inc.691@gmail.com>
2021-03-10 17:17:47 -0300

    create FaqContent component

---
+++ web/src/Pages/Home/Components/FaqContent/styles.ts
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+export const FaqContentContainer = styled.li<{ active: boolean }>`
+    width: 100%;
+    max-width: 815px;
+
+    list-style: none;
+
+    overflow: hidden;
+
+    .always-shown {
+        padding: .8em 2.2em .8em 1.2em;
+
+        font-size: 1.5rem;
+
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        background: #303030;
+        border-bottom: 1px solid #000;
+
+        width: 100%;
+
+        cursor: pointer;
+        transition: .3s;
+
+        h4 {
+            font-weight: 400;
+        }
+
+        .icon {
+            transition: .3s;
+
+            transform: rotate(${ props => props.active ? '45deg' : '0deg' });
+        }
+    }
+
+    .content-togglable {
+        max-height: ${ props => props.active ? '800' : '0' }px;    
+
+        transition: max-height .4s;
+
+        p {
+            background: #303030;
+            
+            padding: 1.2em;
+
+            font-size: 1.625rem;
+            font-weight: 400;
+        }
+    }
+
+    & + & {
+        margin-top: 8px;
+    }
+`;
--- web/src/Pages/Home/index.tsx
+++ web/src/Pages/Home/index.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import { FiChevronRight } from 'react-icons/fi';
+import React, { useState } from 'react';
+import { FiChevronRight, FiPlus } from 'react-icons/fi';
 import { FaLongArrowAltDown } from 'react-icons/fa';
 
 import Tv from '../../assets/tv.png';
 import Mobile from '../../assets/mobile.jpg';
 
-import { FirstSection, Container, ContentContainer } from './styles';
+import { FirstSection, Container, ContentContainer, FaqContainer } from './styles';
 
 import Header from './Components/Header';
 import ContentDescription from './Components/ContentDescription';
 import { blueText } from '../../styles/variables/colors';
+import FaqContent from './Components/FaqContent';
 
 const Home: React.FC = () => {
+    const [ active, setActive ] = useState(false);
+
     return (
         <Container>
             <FirstSection>
@@ -42,7 +45,7 @@ const Home: React.FC = () => {
 
                 <section className="tv-animation image-wrapper">
                     <img src={ Tv } alt="tv" />
-                    <div className="video-wrapper">
+                    <div className="video-wrapper vw1">
                         <video 
                             src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-tv-0819.m4v" 
                             autoPlay
@@ -77,6 +80,65 @@ const Home: React.FC = () => {
                     titleDescription="Salve seus títulos favoritos e sempre tenha algo para assistir."
                 />
             </ContentContainer>
+            
+            <ContentContainer>
+                <ContentDescription 
+                    title="Assista quando quiser."
+                    titleDescription="Assista no celular, tablet, Smart TV ou notebook sem pagar a mais por isso."
+                />
+
+                <section className="tv-animation many-devices">
+                    <img src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile.png" alt="thumbnail"/>
+                
+                    <div className="video-wrapper vw2">
+                        <video 
+                            src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices.m4v"
+                            autoPlay
+                            muted
+                            loop
+                            playsInline
+                        />
+                    </div>
+                </section>
+            </ContentContainer>
+        
+            <FaqContainer>
+                <h1>Perguntas frequentes</h1>
+
+                <ul>
+                    <FaqContent title="O que é AniWatch?" >
+                        O AniWatch é um serviço de transmissão online que oferece uma ampla variedade de animes, filmes sobre animes e documentários sobre animes premiados em milhares de aparelhos conectados à internet.
+                        <br/><br/>
+                        Você pode assistir a quantos animes quiser, quando e onde quiser, sem comerciais – tudo por um preço mensal bem acessível. Aqui você sempre encontra novidades. A cada semana, adicionamos novas séries e filmes.
+                    </FaqContent>
+                    <FaqContent title="Quanto custa o AniWatch?" >
+                        Assista ao AniWatch no seu celular, tablet, Smart TV, notebook ou aparelho de streaming por uma taxa mensal única. Os planos variam de R$21,90 a R$45,90 por mês. Sem contrato nem taxas extras.
+                    </FaqContent>
+                    <FaqContent title="Onde posso assistir?" >
+                        Assista onde quiser, o quanto quiser e em um número ilimitado de aparelhos. Faça login com sua conta AniWatch em aniwatch.com para começar a assistir no computador ou em qualquer aparelho conectado à Internet compatível com o aplicativo AniWatch, como Smart TVs, smartphones, tablets, aparelhos de streaming e videogames.
+                        <br/><br/>
+                        Você também pode baixar a sua série favorita com o aplicativo AniWatch para iOS, Android ou Windows 10. Use downloads para levar o AniWatch para onde quiser sem precisar de conexão com a Internet. Leve o AniWatch com você para qualquer lugar.
+                    </FaqContent>
+                    <FaqContent title="Como faço para cancelar?" >
+                        O AniWatch é flexível. Não há contratos nem compromissos. Você pode cancelar a sua conta na internet com apenas dois cliques. Não há taxa de cancelamento – você pode começar ou encerrar a sua assinatura a qualquer momento.
+                    </FaqContent>
+                    <FaqContent title="O que eu posso assistir no AniWatch?" >
+                        O AniWatch tem um grande catálogo de animes, documentários e originais AniWatch premiados e muito mais. Assista o quanto quiser, quando quiser.
+                    </FaqContent>
+                </ul>
+            
+                <div className="sign-wrapper">
+                    <h6>Pronto para assistir? Informe seu email para criar ou reiniciar sua assinatura.</h6>
+                    
+                    <div className="input-box">
+                            <input type="text" placeholder="Entrar" />
+                            <button>
+                                Vamos lá
+                                <FiChevronRight className="icon" />
+                            </button>
+                    </div>
+                </div>
+            </FaqContainer>
         </Container>
     );
 }
