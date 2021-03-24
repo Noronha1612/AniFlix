a4548de7b365ecada7b92a495eecc5b090d24512
NoronhaGCP <inc.691@gmail.com>
2021-03-24 18:57:45 -0300

    add login component to routes

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import Home from './Pages/Home';
+import SignIn from './Pages/SignIn';
 
 const Routes: React.FC = () => {
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={ Home } />
+                <Route path="/login" exact component={ SignIn } />
             </Switch>
         </BrowserRouter>
     );
