b42d1d1d7d5b55653102a4d97ab9b6e14e2ff1fb
NoronhaGCP <inc.691@gmail.com>
2021-03-25 18:23:59 -0300

    Inserted logo as link to homepage

--- web/src/Pages/SignIn/index.tsx
+++ web/src/Pages/SignIn/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Container } from './styles';
+import { Container, LoginBody, LoginContainer } from './styles';
+
+import LogoSvg from '../../assets/logo.svg';
 
 const SignIn: React.FC = () => {
     return (
         <Container>
-            Home Page
+            <Link to="/">
+                <img src={ LogoSvg } alt=""/>
+            </Link>
+
+            
         </Container>
     );
 }
